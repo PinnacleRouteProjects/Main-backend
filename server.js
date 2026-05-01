cfcb4373c264dfc2695bce8858131b34317dd4f9
PinnacleRouteProjects <admin@pinnacleroute.com>
2026-05-01 13:28:59 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,19 +2,6 @@ const app = require('./app');
 
 const PORT = process.env.PORT || 5000;
 
-const startServer = (port, attemptsLeft = 10) => {
-  const server = app.listen(port, () => {
-    console.log(`Server running on port ${port}`);
-  });
-
-  server.on('error', (error) => {
-    if (error.code === 'EADDRINUSE' && attemptsLeft > 0) {
-      startServer(port + 1, attemptsLeft - 1);
-      return;
-    }
-
-    throw error;
-  });
-};
-
-startServer(PORT);
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
